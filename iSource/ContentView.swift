9cb2b68e821ef9d23f88b318176b3df0541fef4f
Rabialco Argana <rabialco@gmail.com>
2022-07-27 14:22:17 +0700

    Initial Commit

---
+++ iSource/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iSource
+//
+//  Created by Rabialco Argana on 27/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iSource/iSourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSourceApp.swift
+//  iSource
+//
+//  Created by Rabialco Argana on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iSourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
